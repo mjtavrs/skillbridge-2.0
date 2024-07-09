b42818c7d6cee52a62be911aa374d1beb6267d43
Marcos Tavares <mjtdes.md@gmail.com>
2024-07-09 09:31:25 -0300

    refactor: Adjust the AboutUsCta component.

--- src/app/about-us/AboutUsSections/components/AboutUsCta.tsx
+++ src/app/about-us/AboutUsSections/components/AboutUsCta.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image'
 
 export function AboutUsCta() {
   return (
-    <div className="relative flex items-center gap-72 rounded-xl bg-white p-20">
-      <div className="z-10 w-[63rem] space-y-5">
-        <p className="text-5xl font-semibold leading-[120%] text-skillbridge-grey-15">
+    <div className="relative flex items-center justify-between rounded-xl bg-white p-20 xl:p-[3.75rem]">
+      <div className="z-10 w-[63rem] space-y-5 xl:w-[47rem] xl:space-y-3">
+        <p className="text-5xl font-semibold leading-[120%] text-skillbridge-grey-15 xl:text-[2.375rem]">
           <span className="text-skillbridge-orange-50">Together</span>,
           let&apos;s shape the future of digital innovation
         </p>
-        <p className="text-lg text-skillbridge-grey-30">
+        <p className="text-lg text-skillbridge-grey-30 xl:text-base">
           Join us on this exciting learning journey and unlock your potential in
           design and development.
         </p>
@@ -19,7 +19,7 @@ export function AboutUsCta() {
         alt=""
         width={360}
         height={360}
-        className="absolute -top-5 right-60"
+        className="absolute -top-4 right-60 xl:-top-9"
       />
       <Button href="/courses" variant="orange">
         Join Now
